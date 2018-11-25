907e7baa3585a4fa92df583de5d4b0a9e371733f
Renato <serra.renato@gmail.com>
2018-11-25 21:32:29 -0200

    restriction

--- src/main/java/br/com/yellowcar/domain/restriction/MaxDistanceRestriction.java
+++ src/main/java/br/com/yellowcar/domain/restriction/MaxDistanceRestriction.java
@@ -3,10 +3,11 @@ package br.com.yellowcar.domain.restriction;
 import br.com.yellowcar.domain.exception.RestrictionException;
 import br.com.yellowcar.domain.mobile.Cab;
 import br.com.yellowcar.domain.mobile.Passenger;
+import br.com.yellowcar.domain.mobile.World;
 
 public class MaxDistanceRestriction extends RestrictionChain{
 
-	Integer maxDistance = World.
+	Integer maxDistance;
 	@Override
 	public void isPossible(Cab cab, Passenger passenger) throws RestrictionException {
 		// TODO Auto-generated method stub
